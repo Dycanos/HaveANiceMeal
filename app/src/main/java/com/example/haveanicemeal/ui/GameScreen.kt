83cd4c8c7a1c3f18ef4e27a3019774554633c807
Dycanos <ljacques54450@gmail.com>
2024-04-05 14:27:36 +0200

    🎉 - Initialisation du projet d'entrainement

---
+++ app/src/main/java/com/example/haveanicemeal/ui/GameScreen.kt
@@ -0,0 +1,21 @@
+package com.example.haveanicemeal.ui
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun MealAppBar(){
+
+}
+
+@Composable
+fun GameScreen(){
+}
+
+@Preview
+@Composable
+fun GameScreenPreview(){
+    GameScreen()
+}
---
+++ app/src/main/java/com/example/haveanicemeal/ui/HomePage.kt
@@ -0,0 +1,16 @@
+package com.example.haveanicemeal.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomePageScreen(
+    onNextButtonClicked: (Int) -> Unit,
+    modifier: Modifier = Modifier) {
+
+
+    Column {
+
+    }
+}
